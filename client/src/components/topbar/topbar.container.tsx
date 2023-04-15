3839647368ed731abd0fd88a12c7f70e39b0826f
Manish Gandotra <Manishgandotra@icloud.com>
2023-04-15 22:43:07 +0530

    toolbar

--- client/src/components/topbar/topbar.container.tsx
+++ client/src/components/topbar/topbar.container.tsx
@@ -1,4 +1,138 @@
-const TopbarComponentContainer =() => {
-    return <>TopbarComponentContainer</>
+
+import * as React from 'react';
+import {
+    AppBar,
+    Box,
+    Toolbar,
+    IconButton,
+    Typography,
+    MenuItem,
+    Menu
+} from '@mui/material';
+import { AccountCircle } from '@mui/icons-material';
+import MoreIcon from '@mui/icons-material/MoreVert';
+import AppIcon from "../../images/main_icon.svg"
+import "./topbar.scss"
+import * as colors from "../../commonscss/color"
+const color = colors.default
+export default function TopbarComponentContainer(props: any) {
+
+
+    const menuId = 'primary-search-account-menu';
+    const renderMenu = (
+        <Menu
+            anchorEl={props.anchorEl}
+            anchorOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+            }}
+            id={menuId}
+            keepMounted
+            transformOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+            }}
+            open={props.isMenuOpen}
+            onClose={props.handleMenuClose}
+        >
+            <MenuItem onClick={props.handleMenuClose}>Profile</MenuItem>
+            <MenuItem onClick={props.handleMenuClose}>Logout</MenuItem>
+        </Menu>
+    );
+
+    const mobileMenuId = 'primary-search-account-menu-mobile';
+    const renderMobileMenu = (
+        <Menu
+            anchorEl={props.mobileMoreAnchorEl}
+            anchorOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+            }}
+            id={mobileMenuId}
+            keepMounted
+            transformOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+            }}
+            open={props.isMobileMenuOpen}
+            onClose={props.handleMobileMenuClose}
+        >
+
+            <MenuItem onClick={props.handleMobileMenuClose}>
+                <IconButton
+                    size="large"
+                    aria-label="account of current user"
+                    aria-controls="primary-search-account-menu"
+                    aria-haspopup="true"
+                    color="inherit"
+                >
+                    <AccountCircle className='icon' />
+                </IconButton>
+                <p>Profile</p>
+            </MenuItem>
+            <MenuItem onClick={props.handleMobileMenuClose}>
+                <IconButton
+                    size="large"
+                    aria-label="account of current user"
+                    aria-controls="primary-search-account-menu"
+                    aria-haspopup="true"
+                    color="inherit"
+                >
+                    <AccountCircle sx={{ color: color.main_color }} />
+                </IconButton>
+                <p>Logout</p>
+            </MenuItem>
+        </Menu>
+    );
+
+    return (
+        <Box sx={{ flexGrow: 1 }}>
+            <AppBar position="static" style={{ background: '#FFF' }}>
+                <Toolbar variant="dense">
+                    <img className="profile" src={AppIcon} />
+                    <Typography
+                        variant="h6"
+                        noWrap
+                        className="app_name"
+                        component="div"
+                        sx={{ display: { xs: 'none', sm: 'block' } }}
+                    >
+                        Currency Flow
+                    </Typography>
+
+                    <Box sx={{ flexGrow: 1 }} />
+                    <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
+
+
+                        <IconButton
+                            size="large"
+                            edge="end"
+                            aria-label="account of current user"
+                            aria-controls={menuId}
+                            aria-haspopup="true"
+                            onClick={props.handleProfileMenuOpen}
+                            color="inherit"
+                        >
+                            <AccountCircle sx={{ color: color.main_color }} />
+                        </IconButton>
+
+                    </Box>
+                    <Box sx={{ display: { xs: 'flex', md: 'none' } }}>
+                        <IconButton
+                            size="large"
+                            aria-label="show more"
+                            aria-controls={mobileMenuId}
+                            aria-haspopup="true"
+                            onClick={props.handleMobileMenuOpen}
+                            color="inherit"
+                        >
+                            <MoreIcon sx={{ color: color.main_color }} />
+                        </IconButton>
+                    </Box>
+                </Toolbar>
+            </AppBar>
+            {renderMobileMenu}
+            {renderMenu}
+        </Box>
+    );
 }
-export default TopbarComponentContainer;
--- client/src/components/topbar/topbar.helper.tsx
+++ client/src/components/topbar/topbar.helper.tsx
@@ -1,5 +1,37 @@
+import * as React from 'react';
+
 import ToolbarComponentComponent from "./topbar.container"
 const TopbarComponentHelper = () => {
-    return <ToolbarComponentComponent />
+    const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+    const [mobileMoreAnchorEl, setMobileMoreAnchorEl] =
+        React.useState<null | HTMLElement>(null);
+
+    const isMenuOpen = Boolean(anchorEl);
+    const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
+
+    const handleProfileMenuOpen = (event: React.MouseEvent<HTMLElement>) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleMobileMenuClose = () => {
+        setMobileMoreAnchorEl(null);
+    };
+
+    const handleMenuClose = () => {
+        setAnchorEl(null);
+        handleMobileMenuClose();
+    };
+
+    const handleMobileMenuOpen = (event: React.MouseEvent<HTMLElement>) => {
+        setMobileMoreAnchorEl(event.currentTarget);
+    };
+
+    return <ToolbarComponentComponent 
+        isMobileMenuOpen ={isMobileMenuOpen}
+        isMenuOpen ={isMenuOpen}
+        handleMobileMenuClose={handleMobileMenuClose}
+        handleProfileMenuOpen={handleProfileMenuOpen}
+        handleMenuClose={handleMenuClose}
+        handleMobileMenuOpen={handleMobileMenuOpen} />
 }
 export default TopbarComponentHelper;
